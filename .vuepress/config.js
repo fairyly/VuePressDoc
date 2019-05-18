c2bd44eb85ea6f6f5341c486628c1f0c7b89990e
fairyly <fairyilys@gmail.com>
2019-05-18 17:40:45 +0800

    feat: 初始化 VuePress 文档项目，搭建基本框架

---
+++ .vuepress/config.js
@@ -0,0 +1,60 @@
+module.exports = {
+  title: '大前端',
+  description: '大前端',
+  // 注入到当前页面的 HTML <head> 中的标签
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+  ],
+  // base: '/', // 这是部署到github相关的配置 下面会讲
+  markdown: {
+    lineNumbers: true // 代码块显示行号
+  },
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      {
+        text: 'Blog',
+        items: [
+          { text: '2019', link: '/blog/2019/' },
+          { text: '2018', link: '/blog/2018/' },
+          { text: '2017', link: '/blog/2017/' },
+          { text: '2016', link: '/blog/2016/' }
+        ]
+      },
+      {
+        text: '选择语言',
+        items: [
+          { text: '中文', link: '/guide/' },
+          { text: 'Japanese', link: '/language/japanese' }
+        ]
+      },
+      { text: 'Github', link: 'https://github.com/fairyly/VuePressDoc' },
+    ],
+    sidebar: {
+      '/guide/': [
+        ''     /* /guide/ */
+      ],
+      '/blog/2016/': [   /* /blog/ */
+        '',
+        '201601',
+        '201602'
+      ],
+      '/blog/2017/': [
+        '',
+        '201701',
+        '201702'
+      ],
+      '/blog/2018/': [
+        '',
+        '201801',
+        '201802'
+      ],
+      '/blog/2019/': [
+        '', 
+        '201901',
+        '201902'
+      ]
+    }
+}
+};
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# VuePressDoc
-基于 VuePress 的文档
+---
+home: true
+heroImage: /logo.png
+actionText: 快速上手 →
+actionLink: /guide/
+features:
+- title: 简洁至上
+  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+- title: Vue驱动
+  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
+- title: 高性能
+  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+
+footer: MIT Licensed | Copyright © 2019-present fairyly 
+---
+
+
+## 简介
+
+>本文档使用 `VuePress 1.x` 搭建   
+```
+# 安装
+yarn global add vuepress@next # 或者：npm install -g vuepress@next
+
+# 新建一个 markdown 文件
+echo '# Hello VuePress!' > README.md
+
+# 开始写作
+vuepress dev .
+
+# 构建静态文件
+vuepress build .
+```
+
